d1ec7241b81f146234db871d543e23ca44f3cfa7
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-06-28 03:01:18 -0700

    make a footer section

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,16 +9,17 @@ const sizeClasses = {
 const Button = ({
   icon: Icon,
   text,
-  size = "medium",
+  size = "medium", 
   bgColor = "bg-blue-500",
   textColor = "text-white",
   onClick,
+ 
 }) => {
   return (
     <div className="p-1 hover:bg-[#e1dcef] rounded-xl">
       <button
         onClick={onClick}
-        className={`${sizeClasses[size]} ${bgColor} ${textColor} hover:bg-purple-900 hover:text-white font-medium  rounded-xl border-2 border-gray-400 flex gap-1 sm:gap-2 items-center duration-300`}
+        className={`${sizeClasses[size]} ${bgColor} ${textColor}   hover:bg-purple-900 hover:text-white font-medium  rounded-xl border-2 border-gray-400 flex gap-1 sm:gap-2 items-center duration-300`}
       >
         {Icon && <Icon className="text-xs sm:text-xl " />}
         {text}
